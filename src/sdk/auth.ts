8a3e4a35f9e720dea774854e5622876941d738fb
Ray Epps <rayharryepps@gmail.com>
2023-03-06 12:31:54 -0700

    add url and scopes to oauth config

--- src/sdk/auth.ts
+++ src/sdk/auth.ts
@@ -1,8 +1,13 @@
 import { OAuth2AuthConfig, StandardAuthConfig } from './types';
 
 export const auth = {
-  oauth2: (config: Omit<OAuth2AuthConfig, 'type'>): OAuth2AuthConfig => ({
+  oauth2: (
+    config: Omit<OAuth2AuthConfig, 'type' | 'url'> & {
+      url?: OAuth2AuthConfig['url'];
+    },
+  ): OAuth2AuthConfig => ({
     ...config,
+    url: config.url ?? (() => config.tokenUrl),
     type: 'oauth2',
   }),
   standard: (config: Omit<StandardAuthConfig, 'type'>): StandardAuthConfig => ({
