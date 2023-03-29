a2b596fe692b37f16e6981e6194119282d043181
Zaran Lalvani <zaran@vessel.land>
2023-03-29 19:25:10 -0400

    fix types/jest and jest incompatibility

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts', '!*.md'],
+  entry: ['src/index.ts', '!*.md', '!*.test.ts'],
   sourcemap: true,
   clean: true,
   dts: true,
