704b4d84bc437609fa443052492b8e311d0b7916
zkirby <zkirby16@gmail.com>
2023-03-30 23:19:49 -0700

    export actions

--- src/platforms/apollo/index.ts
+++ src/platforms/apollo/index.ts
@@ -1,6 +1,38 @@
 import { auth, platform } from '../../sdk';
+import { client } from './client';
+import * as constants from './constants';
 import { icon } from './icon';
 
+import createAccount from './actions/accounts/create';
+import listAccounts from './actions/accounts/list';
+import searchAccount from './actions/accounts/search';
+import updateAccount from './actions/accounts/update';
+
+import listActivities from './actions/activities/list';
+
+import createContact from './actions/accounts/create';
+import listContacts from './actions/accounts/list';
+import searchContact from './actions/accounts/search';
+import updateContact from './actions/accounts/update';
+
+import createCustomField from './actions/customFields/create';
+import listCustomFields from './actions/customFields/list';
+
+import searchEmails from './actions/emails/search';
+
+import listEmailAccounts from './actions/emailAccounts/list';
+
+import createSequences from './actions/sequences/create';
+import listSequences from './actions/sequences/list';
+import startSequence from './actions/sequences/start';
+
+import createSequenceSteps from './actions/sequenceSteps/create';
+
+import createSequenceTemplates from './actions/sequenceTemplates/create';
+
+import listUsers from './actions/users/list';
+
+export * as types from './schemas';
 export default platform('apollo', {
   auth: [
     auth.apiToken({
@@ -14,7 +46,36 @@ export default platform('apollo', {
     name: 'Apollo',
     iconURI: icon,
   },
-  constants: {},
-  client: {} as any,
-  actions: {},
+  constants,
+  client,
+  actions: {
+    createAccount,
+    listAccounts,
+    updateAccount,
+    searchAccount,
+
+    listActivities,
+
+    createContact,
+    listContacts,
+    updateContact,
+    searchContact,
+
+    createCustomField,
+    listCustomFields,
+
+    searchEmails,
+
+    listEmailAccounts,
+
+    createSequences,
+    listSequences,
+    startSequence,
+
+    createSequenceSteps,
+
+    createSequenceTemplates,
+
+    listUsers,
+  },
 });
