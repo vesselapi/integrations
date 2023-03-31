03c9254dfeeffb3e82ed3f1b77375704af0cd3bd
zkirby <zkirby16@gmail.com>
2023-03-31 16:55:55 -0700

    add types

--- src/platforms/index.ts
+++ src/platforms/index.ts
@@ -3,6 +3,7 @@ import apollo from '@/platforms/apollo';
 import dialpad from '@/platforms/dialpad';
 import outreach from '@/platforms/outreach';
 import ringcentral from '@/platforms/ringcentral';
+import salesloft from '@/platforms/salesloft';
 
 export const integrations = {
   outreach,
@@ -10,11 +11,13 @@ export const integrations = {
   dialpad,
   apollo,
   ringcentral,
+  salesloft,
 };
 export { types as aircallTypes } from '@/platforms/aircall';
 export { types as apolloTypes } from '@/platforms/apollo';
 export { types as dialpadTypes } from '@/platforms/dialpad';
 export { types as outreachTypes } from '@/platforms/outreach';
 export { types as ringcentralTypes } from '@/platforms/ringcentral';
+export { types as salesloftTypes } from '@/platforms/salesloft';
 
 export const integrationsList = Object.values(integrations);
