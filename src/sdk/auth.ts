04cc7ce89eb7842e98205f23c451d415077d774c
Ray Epps <rayharryepps@gmail.com>
2023-03-31 13:56:07 -0600

    dynamic url + fix url function (#51)

--- src/sdk/auth.ts
+++ src/sdk/auth.ts
@@ -1,3 +1,4 @@
+import { isFunction, isString } from 'radash';
 import {
   AuthQuestion,
   HttpsUrl,
@@ -6,22 +7,34 @@ import {
   StandardAuthConfig,
 } from './types';
 
+const toQueryString = (query: Record<string, string>): string => {
+  const params = new URLSearchParams();
+  for (const [key, value] of Object.entries(query)) {
+    params.set(key, value);
+  }
+  return params.toString();
+};
+
 export const auth = {
   oauth2: (options: {
-    authUrl: HttpsUrl;
-    tokenUrl: HttpsUrl;
+    authUrl: HttpsUrl | OAuth2AuthConfig['authUrl'];
+    tokenUrl: HttpsUrl | OAuth2AuthConfig['tokenUrl'];
+    questions?: AuthQuestion[];
     default?: boolean;
     scopeSeparator?: OAuth2AuthConfig['scopeSeparator'];
     tokenAuth?: OAuth2AuthConfig['tokenAuth'];
-    questions?: AuthQuestion[];
     oauthBodyFormat?: OAuth2AuthConfig['oauthBodyFormat'];
     url?: OAuth2AuthConfig['url'];
     isRetryable?: RetryableCheckFunction;
     display?: OAuth2AuthConfig['display'];
   }): OAuth2AuthConfig => ({
     type: 'oauth2',
-    authUrl: options.authUrl,
-    tokenUrl: options.tokenUrl,
+    authUrl: isString(options.authUrl)
+      ? () => options.authUrl as HttpsUrl
+      : options.authUrl,
+    tokenUrl: isString(options.tokenUrl)
+      ? () => options.tokenUrl as HttpsUrl
+      : options.tokenUrl,
     tokenAuth: options.tokenAuth ?? 'body',
     default: options.default ?? false,
     scopeSeparator: options.scopeSeparator ?? ' ',
@@ -35,16 +48,19 @@ export const auth = {
     },
     url:
       options.url ??
-      (({ scopes, clientId, redirectUrl, state }) => {
-        const query = [
-          ['client_id', clientId],
-          ['redirect_uri', redirectUrl],
-          ['scope', scopes.join(options.scopeSeparator ?? '+')],
-          ['state', state],
-        ]
-          .map((x) => x.join('='))
-          .join('&');
-        return `${options.authUrl}?${query}`;
+      (({ scopes, clientId, redirectUrl, state, answers }) => {
+        const query: Record<string, string> = {
+          client_id: clientId,
+          redirect_uri: redirectUrl,
+          scope: scopes.join(options.scopeSeparator ?? '+'),
+          state,
+          response_type: 'code',
+        };
+        return `${
+          isFunction(options.authUrl)
+            ? options.authUrl({ answers })
+            : options.authUrl
+        }?${toQueryString(query)}`;
       }),
     isRetryable:
       options.isRetryable ?? (({ response }) => response.status === 401),
