1361d5d1a6b8ec1c503951901e8c3d3ea3b8a0b1
zkirby <zkirby16@gmail.com>
2023-04-01 13:14:09 -0700

    merge

--- src/platforms/index.ts
+++ src/platforms/index.ts
@@ -3,7 +3,6 @@ import apollo from '@/platforms/apollo';
 import dialpad from '@/platforms/dialpad';
 import outreach from '@/platforms/outreach';
 import ringcentral from '@/platforms/ringcentral';
-import salesloft from '@/platforms/salesloft';
 
 export { default as aircall, types as aircallTypes } from '@/platforms/aircall';
 export { default as apollo, types as apolloTypes } from '@/platforms/apollo';
@@ -23,26 +22,4 @@ export const integrationsList = Object.values([
   dialpad,
   outreach,
   ringcentral,
-<<<<<<< HEAD
-  salesloft,
-};
-export { types as aircallTypes } from '@/platforms/aircall';
-export { types as apolloTypes } from '@/platforms/apollo';
-export { types as dialpadTypes } from '@/platforms/dialpad';
-export { types as outreachTypes } from '@/platforms/outreach';
-export { types as ringcentralTypes } from '@/platforms/ringcentral';
-export { types as salesloftTypes } from '@/platforms/salesloft';
-
-export const integrationsList = Object.values(integrations);
-||||||| 4f67c12
-};
-export { types as aircallTypes } from '@/platforms/aircall';
-export { types as apolloTypes } from '@/platforms/apollo';
-export { types as dialpadTypes } from '@/platforms/dialpad';
-export { types as outreachTypes } from '@/platforms/outreach';
-export { types as ringcentralTypes } from '@/platforms/ringcentral';
-
-export const integrationsList = Object.values(integrations);
-=======
 ]);
->>>>>>> main
