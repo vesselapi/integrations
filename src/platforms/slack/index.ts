c61cb4f6a12065b1a649fa4951bb187ede4aef90
Zaran Lalvani <zaran@vessel.dev>
2023-04-04 16:04:13 -0500

    fix: misc slack fixes (#73)

--- src/platforms/slack/index.ts
+++ src/platforms/slack/index.ts
@@ -5,17 +5,22 @@ import listUsers from '@/platforms/slack/actions/users/list';
 import { client } from '@/platforms/slack/client';
 import * as constants from '@/platforms/slack/constants';
 import { icon } from '@/platforms/slack/icon';
+import { slackExpiredAuth } from '@/platforms/slack/schemas';
 import { auth, platform } from '@/sdk';
 
 export * as types from '@/platforms/slack/schemas';
 
 export default platform('slack', {
   auth: auth.oauth2({
-    authUrl: 'https://slack.com/oauth/authorize',
-    tokenUrl: 'https://slack.com/api/oauth.access',
+    authUrl: 'https://slack.com/oauth/v2/authorize',
+    tokenUrl: 'https://slack.com/api/oauth.v2.access',
     oauthBodyFormat: 'form',
-    scopeSeparator: ' ',
+    scopeSeparator: ',',
     default: true,
+    isRetryable: async ({ response }) => {
+      const zodResult = slackExpiredAuth.safeParse(await response.json());
+      return zodResult.success;
+    },
   }),
   display: {
     name: 'Slack',
