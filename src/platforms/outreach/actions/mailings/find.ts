3b1efd337f79a7da4d7aad236fcffab459ca5c5c
zkirby <zkirby16@gmail.com>
2023-04-03 23:03:10 -0500

    fix mailings

--- src/platforms/outreach/actions/mailings/find.ts
+++ src/platforms/outreach/actions/mailings/find.ts
@@ -14,6 +14,6 @@ export default action(
     scopes: [],
   },
   async ({ input, auth }) => {
-    return await client.mailings.get(auth, { id: input.id });
+    return await client.mailings.find(auth, { id: input.id });
   },
 );
