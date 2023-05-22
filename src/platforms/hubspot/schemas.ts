f914d94cca91c79d9bf3dd715a3b054630c999e6
Manas <111779510+manas-vessel@users.noreply.github.com>
2023-05-22 17:32:49 -0500

    fix: [hubspot] upsert response schema should only contain id (#126)
    * fix: [hubspot] upsert response schema should only contain id
    
    * removing passthrough

--- src/platforms/hubspot/schemas.ts
+++ src/platforms/hubspot/schemas.ts
@@ -96,6 +96,10 @@ export const baseHubspotObjectSchema = <T extends z.ZodSchema>(properties: T) =>
     })
     .passthrough();
 
+export const upsertResponseSchema = z.object({
+  id: hubspotIdSchema,
+});
+
 // -
 // Owners
 // -
