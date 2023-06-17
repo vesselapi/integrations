a8f0e6f09cb55c41a0fd4d62db9b86f0fca0245d
Manas <111779510+manas-vessel@users.noreply.github.com>
2023-06-16 20:16:42 -0500

    fix: sf client validators (#165)

--- src/platforms/salesforce/schemas.ts
+++ src/platforms/salesforce/schemas.ts
@@ -175,8 +175,12 @@ export const salesforceAccount = validators
     CreatedDate: validators.date(),
     LastModifiedDate: validators.date(),
     OwnerId: z.string().nullable(),
-    Contacts: z.object({ records: z.array(z.object({ Id: z.string() })) }),
-    Opportunities: z.object({ records: z.array(z.object({ Id: z.string() })) }),
+    Contacts: z
+      .object({ records: z.array(z.object({ Id: z.string() })) })
+      .nullable(),
+    Opportunities: z
+      .object({ records: z.array(z.object({ Id: z.string() })) })
+      .nullable(),
   })
   .partial()
   .required(requiredFields);
@@ -240,7 +244,7 @@ export const salesforceOpportunity = validators
     Id: z.string(),
     Name: z.string().nullable(),
     StageName: z.string().nullable(),
-    Amount: z.string().nullable(),
+    Amount: z.union([z.number(), z.string()]).nullable(),
     CloseDate: validators.date().nullable(),
     Probability: z.string().nullable(),
     ExpectedRevenue: z.string().nullable(),
@@ -436,9 +440,11 @@ export const salesforceContentNote = validators
     TextPreview: z.string().nullable(),
     Content: z.string().nullable(), // url to fetch the Content Body.
     OwnerId: z.string().nullable(),
-    ContentDocumentLinks: z.object({
-      records: z.array(salesforceContentDocumentLink),
-    }),
+    ContentDocumentLinks: z
+      .object({
+        records: z.array(salesforceContentDocumentLink),
+      })
+      .nullable(),
   })
   .partial()
   .required(requiredFields);
@@ -489,8 +495,8 @@ export const salesforceTask = validators
       .transform((v) => v as SalesforceCallType)
       .nullable(),
     TaskSubtype: z.string().nullable(),
-    Who: z.object({ Id: z.string(), Type: z.string() }).nullable(),
-    What: z.object({ Id: z.string(), Type: z.string() }).nullable(),
+    Who: z.object({ Id: z.string(), Type: z.string().optional() }).nullable(),
+    What: z.object({ Id: z.string(), Type: z.string().optional() }).nullable(),
   })
   .partial()
   .required(requiredFields);
@@ -567,22 +573,24 @@ export const salesforceEvent = validators
     Who: z
       .object({
         Id: z.string(),
-        Type: z.string(),
+        Type: z.string().optional(),
       })
       .nullable(),
     What: z
       .object({
         Id: z.string(),
-        Type: z.string(),
+        Type: z.string().optional(),
       })
       .nullable(),
-    EventRelations: z.object({
-      records: z.array(
-        z.object({
-          Id: z.string(),
-        }),
-      ),
-    }),
+    EventRelations: z
+      .object({
+        records: z.array(
+          z.object({
+            Id: z.string(),
+          }),
+        ),
+      })
+      .nullable(),
   })
   .partial()
   .required(requiredFields);
@@ -695,10 +703,12 @@ export const salesforceEmailMessage = validators
     CreatedById: z.string().nullable(),
     IsArchived: z.boolean().nullable(),
     IsDeleted: z.boolean().nullable(),
-    RelatedTo: z.object({
-      Id: z.string(),
-      Type: z.string(),
-    }),
+    RelatedTo: z
+      .object({
+        Id: z.string(),
+        Type: z.string().optional(),
+      })
+      .nullable(),
   })
   .partial()
   .required({
