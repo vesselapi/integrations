9aa642db68758b5358e835addb5f099e44a4cf41
Avery Yip <12945091+averyyip@users.noreply.github.com>
2023-06-21 12:29:19 -0700

    feat: Add freshsales (#159)
    * add freshsales
    
    * Fix mc

---
+++ src/platforms/freshsales/index.ts
@@ -0,0 +1,23 @@
+import { auth, platform } from '@/sdk';
+import { client } from './client';
+import { icon } from './icon';
+
+export default platform('freshsales', {
+  auth: auth.apiToken({
+    questions: [
+      { type: 'text', id: 'subdomain', label: 'Organization Subdomain' },
+    ],
+    display: {
+      markdown: () =>
+        `- Login to Freshsales to find your Organization Subdomain. In your address bar, the subdomain is the first part of the URL. For example, if your URL is https://example-org.myfreshworks.com, your subdomain is "example-org" without quotes.`,
+    },
+  }),
+  display: {
+    name: 'Freshsales',
+    iconURI: icon,
+    categories: ['crm'],
+  },
+  client,
+  constants: {},
+  actions: {},
+});
