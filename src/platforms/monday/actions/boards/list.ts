25919f653908a7e82c2b4cafdca9f7e6fa91d06b
Manas Verma <manas@vessel.land>
2023-06-30 18:27:13 -0500

    feat: adding actions for monday

---
+++ src/platforms/monday/actions/boards/list.ts
@@ -0,0 +1,29 @@
+import { action } from '@/sdk';
+import client from '../../client';
+import { MONDAY_MAX_PAGE_SIZE } from '../../constants';
+import { listInputSchema } from '../../schemas';
+
+export default action(
+  'boards-list',
+  {
+    operation: 'list',
+    resource: 'boards',
+    mutation: true,
+    schema: listInputSchema.partial(),
+    scopes: [],
+  },
+  async ({ auth, input }) => {
+    const limit = input.limit ?? MONDAY_MAX_PAGE_SIZE;
+    const page = input.page ?? 1;
+    const {
+      data: { data },
+      $native,
+    } = await client.boards.list(auth, { limit, page });
+    const nextPageCursor = (data?.boards.length ?? 0) < limit ? null : page + 1;
+    return {
+      boards: data?.boards ?? null,
+      nextPageCursor,
+      $native,
+    };
+  },
+);
---
+++ src/platforms/monday/query-builder.ts
@@ -0,0 +1,59 @@
+import { isNumber } from 'radash';
+import { MondayModule } from './schemas';
+
+export function buildListQuery({
+  module,
+  fields,
+  relationalFields,
+  limit,
+  page,
+}: {
+  module: MondayModule;
+  fields: string[];
+  relationalFields: Record<string, string[]>;
+  limit: number;
+  page: number;
+}) {
+  const queryRelationalFields = Object.entries(relationalFields)
+    .map(([key, value]) => {
+      return `${key} { ${value.join(' ')}}`;
+    })
+    .join(' ');
+  return `query {
+    ${module} (limit: ${limit} page: ${page}) {
+      ${fields.join(' ')}
+      ${queryRelationalFields}
+    }
+  }`;
+}
+
+export function buildCreateQuery({
+  module,
+  metaFields,
+  fields,
+}: {
+  module: MondayModule;
+  metaFields: Record<string, string | number>;
+  fields: Record<string, string | number>;
+}) {
+  const queryMetaFields = Object.entries(metaFields)
+    .map(([key, value]) => {
+      return isNumber(value) ? `${key}: ${value}` : `${key}: "${value}"`;
+    })
+    .join(', ');
+  const queryColumnValues = Object.entries(fields)
+    .map(([key, value]) => {
+      return isNumber(value)
+        ? `\\\"${key}\\\": ${value}`
+        : `\\\"${key}\\\": \\\"${value}\\\"`;
+    })
+    .join(', ');
+  return `mutation {
+    create_${module.replace(/s$/, '')} (
+      ${queryMetaFields},
+      column_values: "{${queryColumnValues}}"
+    ) {
+        id
+    }
+  }`;
+}
