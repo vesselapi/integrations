c2b1c16fab97078aa95094376fbb099166856dc5
zkirby <zkirby16@gmail.com>
2023-07-17 12:58:07 -0700

    clean up Close request formatting

--- src/platforms/close/client.ts
+++ src/platforms/close/client.ts
@@ -4,15 +4,13 @@ import { API_VERSION, BASE_URL } from './constants';
 
 const URL = `${BASE_URL}/${API_VERSION}` as HttpsUrl;
 const request = makeRequestFactory(async (auth, options) => {
+  const base64Key = Buffer.from(await auth.getToken()).toString('base64');
   return {
     ...options,
     url: formatUrl(URL, options.url),
     headers: {
       ...options.headers,
-      Authorization: `Basic ${Buffer.from(
-        await auth.getToken(),
-        'binary',
-      ).toString('base64')}`,
+      Authorization: `Basic ${base64Key}`,
     },
   };
 });
