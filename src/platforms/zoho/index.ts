f2faf68a3cc326fe329f1b74140df5f9cf5c970b
zkirby <zkirby16@gmail.com>
2023-07-17 13:45:38 -0700

    add accounts metadata

--- src/platforms/zoho/index.ts
+++ src/platforms/zoho/index.ts
@@ -1,4 +1,5 @@
 import { auth, HttpsUrl, platform } from '@/sdk';
+import { z } from 'zod';
 import { client } from './client';
 import * as constants from './constants';
 import { icon } from './icon';
@@ -10,6 +11,9 @@ export default platform('zoho', {
       console.log(appMetadata, all);
       return `${appMetadata['accounts-server']}/oauth/v2/token` as HttpsUrl;
     },
+    appMetadataSchema: z.object({
+      'accounts-server': z.string().url(),
+    }),
   }),
   display: { name: 'Zoho', iconURI: icon, categories: ['crm'] },
   constants,
