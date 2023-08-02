b590a60adfc4ef3bfb4264a5a43f30b46bf93d84
Zachary Kirby <zkirby16@gmail.com>
2023-08-02 16:55:14 -0700

    feat: mixpanel client

---
+++ src/platforms/mixpanel/client.ts
@@ -0,0 +1,17 @@
+import { formatUrl, makeRequestFactory } from '@/sdk/client';
+
+const request = makeRequestFactory(async (auth, options) => {
+  
+  return {
+    ...options,
+    url: formatUrl(`https://mixpanel.com`, options.url),
+    headers: {
+      ...options.headers,
+      Authorization: `Basic ${await auth.getToken()}`,
+    },
+  };
+});
+
+export const client = {
+  passthrough: request.passthrough(),
+};
