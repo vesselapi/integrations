319c3efcf6ba9d24ef82714973b5c06e357486ba
Zachary Kirby <zkirby16@gmail.com>
2023-08-03 12:31:15 -0700

    feat: google-mail platform

---
+++ src/platforms/google-mail/index.ts
@@ -0,0 +1,30 @@
+import { client } from '@/platforms/google-mail/client';
+import fullIcon from '@/platforms/google-mail/logos/full';
+import boxIcon from '@/platforms/google-mail/logos/box';
+import { auth, platform } from '@/sdk';
+
+export default platform('google-mail', {
+  auth: auth.oauth2({
+   authUrl: ""https://accounts.google.com/o/oauth2/v2/auth"",
+   tokenUrl: ""https://oauth2.googleapis.com/token"",
+   authParams: {
+      "access_type": "offline",
+      "prompt": "consent"
+}
+    }),
+  display: {
+    name: 'Gmail',
+    logos: {
+     defaultURI: fullIcon ?? boxIcon,
+     fullURI: fullIcon,
+     boxURI: boxIcon,
+    },
+    colors: {
+      primary: '#4285f4',
+    },
+    categories: [],
+  },
+  client,
+  constants: {},
+  actions: {},
+});
