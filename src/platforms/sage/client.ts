59b14aed284b4ae0613334826983ac92848c25ad
Zachary Kirby <zkirby16@gmail.com>
2023-08-03 13:51:39 -0700

    feat: sage client

---
+++ src/platforms/sage/client.ts
@@ -0,0 +1,16 @@
+import { formatUrl, makeRequestFactory } from '@/sdk/client';
+
+const request = makeRequestFactory(async (auth, options) => {
+  return {
+    ...options,
+url: formatUrl(`https://api.accounting.sage.com/v3.1`, options.url),
+    headers: {
+      ...options.headers,
+      Authorization: `Bearer ${await auth.getToken()}`,
+    },
+  };
+});
+
+export const client = {
+  passthrough: request.passthrough(),
+};
