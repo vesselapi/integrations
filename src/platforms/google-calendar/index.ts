183340da1aefd36d3da47bec06d8a87185526d11
Zachary Kirby <zkirby16@gmail.com>
2023-08-03 12:31:10 -0700

    feat: google-calendar platform

---
+++ src/platforms/google-calendar/index.ts
@@ -0,0 +1,30 @@
+import { client } from '@/platforms/google-calendar/client';
+import fullIcon from '@/platforms/google-calendar/logos/full';
+import boxIcon from '@/platforms/google-calendar/logos/box';
+import { auth, platform } from '@/sdk';
+
+export default platform('google-calendar', {
+  auth: auth.oauth2({
+   authUrl: ""https://accounts.google.com/o/oauth2/v2/auth"",
+   tokenUrl: ""https://oauth2.googleapis.com/token"",
+   authParams: {
+      "access_type": "offline",
+      "prompt": "consent"
+}
+    }),
+  display: {
+    name: 'Google Calendar',
+    logos: {
+     defaultURI: fullIcon ?? boxIcon,
+     fullURI: fullIcon,
+     boxURI: boxIcon,
+    },
+    colors: {
+      primary: '#34A853',
+    },
+    categories: [],
+  },
+  client,
+  constants: {},
+  actions: {},
+});
