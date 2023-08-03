1ef8837b811b203dc70b92f4424e34099bc64670
Zachary Kirby <zkirby16@gmail.com>
2023-08-02 20:17:32 -0700

    feat: bitbucket client

---
+++ src/platforms/bitbucket/client.ts
@@ -0,0 +1,16 @@
+import { formatUrl, makeRequestFactory } from '@/sdk/client';
+
+const request = makeRequestFactory(async (auth, options) => {
+  return {
+    ...options,
+    url: formatUrl(`https://bitbucket.${answers.subdomain}.com/rest/api/1.0`, options.url),
+    headers: {
+      ...options.headers,
+      Authorization: `Bearer ${await auth.getToken()}`,
+    },
+  };
+});
+
+export const client = {
+  passthrough: request.passthrough(),
+};
