370d196e08011a3370ac60ab56cf997850a2b28d
Zachary Kirby <zkirby16@gmail.com>
2023-08-03 15:56:49 -0700

    feat: ramp platform

---
+++ src/platforms/ramp/index.ts
@@ -0,0 +1,27 @@
+import { client } from '@/platforms/ramp/client';
+import fullIcon from '@/platforms/ramp/logos/full';
+import boxIcon from '@/platforms/ramp/logos/box';
+import { auth, platform } from '@/sdk';
+
+export default platform('ramp', {
+  auth: auth.oauth2({
+   authUrl: "https://app.ramp.com/v1/authorize",
+   tokenUrl: "https://api.ramp.com/developer/v1/token",
+   tokenAuth: 'header'
+    }),
+  display: {
+    name: 'Ramp',
+    logos: {
+     defaultURI: fullIcon ?? boxIcon,
+     fullURI: fullIcon,
+     boxURI: boxIcon,
+    },
+    colors: {
+      primary: '#787868',
+    },
+    categories: [],
+  },
+  client,
+  constants: {},
+  actions: {},
+});
