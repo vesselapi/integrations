185c1f6f61e9f37870afe89faea0c340f7c9ae60
Zachary Kirby <zkirby16@gmail.com>
2023-08-03 15:56:59 -0700

    feat: survey-monkey platform

---
+++ src/platforms/survey-monkey/index.ts
@@ -0,0 +1,26 @@
+import { client } from '@/platforms/survey-monkey/client';
+import fullIcon from '@/platforms/survey-monkey/logos/full';
+import boxIcon from '@/platforms/survey-monkey/logos/box';
+import { auth, platform } from '@/sdk';
+
+export default platform('survey-monkey', {
+  auth: auth.oauth2({
+   authUrl: "https://api.surveymonkey.com/oauth/authorize",
+   tokenUrl: "https://api.surveymonkey.com/oauth/token"
+    }),
+  display: {
+    name: 'Survey Monkey',
+    logos: {
+     defaultURI: fullIcon ?? boxIcon,
+     fullURI: fullIcon,
+     boxURI: boxIcon,
+    },
+    colors: {
+      primary: '#00bf6f',
+    },
+    categories: [],
+  },
+  client,
+  constants: {},
+  actions: {},
+});
