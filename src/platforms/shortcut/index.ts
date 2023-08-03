c176b61542eaee98cfb211956127be2a277a6d12
Zachary Kirby <zkirby16@gmail.com>
2023-08-02 19:09:31 -0700

    feat: shortcut platform

---
+++ src/platforms/shortcut/index.ts
@@ -0,0 +1,23 @@
+import { client } from '@/platforms/shortcut/client';
+import fullIcon from '@/platforms/shortcut/logos/full';
+import boxIcon from '@/platforms/shortcut/logos/box';
+import { auth, platform } from '@/sdk';
+
+    export default platform('shortcut', {
+      auth: auth.apiToken(),
+      display: {
+        name: 'Shortcut',
+        logos: {
+         defaultURI: fullIcon ?? boxIcon,
+     fullURI: fullIcon,
+     boxURI: boxIcon,
+        },
+        colors: {
+          primary: '#452B5B',
+        },
+        categories: ["other"],
+      },
+      client,
+      constants: {},
+      actions: {},
+    });
