be80f42ea0df0b5f9314fd62a6ede5b445ff4b8a
Zachary Kirby <zkirby16@gmail.com>
2023-08-02 21:51:02 -0700

    feat: factorial platform

---
+++ src/platforms/factorial/index.ts
@@ -0,0 +1,26 @@
+import { client } from '@/platforms/factorial/client';
+import fullIcon from '@/platforms/factorial/logos/full';
+import boxIcon from '@/platforms/factorial/logos/box';
+import { auth, platform } from '@/sdk';
+
+export default platform('factorial', {
+  auth: auth.oauth2({
+   authUrl: "https://api.factorialhr.com/oauth/authorize",
+   tokenUrl: "https://api.factorialhr.com/oauth/token"
+    }),
+  display: {
+    name: 'Factorial',
+    logos: {
+     defaultURI: fullIcon ?? boxIcon,
+     fullURI: fullIcon,
+     boxURI: boxIcon,
+    },
+    colors: {
+      primary: '#007C85',
+    },
+    categories: [],
+  },
+  client,
+  constants: {},
+  actions: {},
+});
