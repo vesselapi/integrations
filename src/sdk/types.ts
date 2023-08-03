8c2f1904ccb815402d949f21a9030d4fb41d4f60
zkirby <zkirby16@gmail.com>
2023-08-03 16:02:10 -0700

    fix issue with auth params

--- src/sdk/types.ts
+++ src/sdk/types.ts
@@ -162,7 +162,7 @@ export type OAuth2AuthConfig<
   /**
    * Additional Auth Params to pass when the `authUrl` is called.
    */
-  authParams: Record<string, string>;
+  authParams: Record<string, any>;
   url: (arg: {
     answers: TAnswers;
     scopes: string[];
