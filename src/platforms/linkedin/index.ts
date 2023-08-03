0d0ee7fbf4b8cc46014248d8be822fa7a5a81a20
Zachary Kirby <zkirby16@gmail.com>
2023-08-03 12:31:41 -0700

    feat: linkedin platform

---
+++ src/platforms/linkedin/index.ts
@@ -0,0 +1,26 @@
+import { client } from '@/platforms/linkedin/client';
+import fullIcon from '@/platforms/linkedin/logos/full';
+import boxIcon from '@/platforms/linkedin/logos/box';
+import { auth, platform } from '@/sdk';
+
+export default platform('linkedin', {
+  auth: auth.oauth2({
+   authUrl: "https://www.linkedin.com/oauth/v2/authorization",
+   tokenUrl: "https://www.linkedin.com/oauth/v2/accessToken"
+    }),
+  display: {
+    name: 'Linkedin',
+    logos: {
+     defaultURI: fullIcon ?? boxIcon,
+     fullURI: fullIcon,
+     boxURI: boxIcon,
+    },
+    colors: {
+      primary: '#0073b1',
+    },
+    categories: [],
+  },
+  client,
+  constants: {},
+  actions: {},
+});
