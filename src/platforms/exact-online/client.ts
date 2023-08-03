343b23326647ac20b8c98458ff304c6c933b5322
Zachary Kirby <zkirby16@gmail.com>
2023-08-02 23:17:36 -0700

    feat: exact-online client

---
+++ src/platforms/exact-online/client.ts
@@ -0,0 +1,17 @@
+import { formatUrl, makeRequestFactory } from '@/sdk/client';;
+
+const request = makeRequestFactory(async (auth, options) => {  
+const { answers } = await auth.getMetadata();
+  return {
+    ...options,
+    url: formatUrl(`https://start.exactonline.${answers.extension}/api`, options.url),
+    headers: {
+      ...options.headers,
+      Authorization: `Bearer ${await auth.getToken()}`,
+    },
+  };
+});
+
+export const client = {
+  passthrough: request.passthrough(),
+};
