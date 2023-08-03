0d44f293e9f79827e516aad4e2cb458a722b26c4
Zachary Kirby <zkirby16@gmail.com>
2023-08-02 17:18:52 -0700

    feat: amplitude platform

---
+++ src/platforms/amplitude/index.ts
@@ -0,0 +1,24 @@
+import { client } from '@/platforms/amplitude/client';
+import fullIcon from '@/platforms/amplitude/icons/full';
+import boxIcon from '@/platforms/amplitude/icons/box';
+import { auth, platform } from '@/sdk';
+
+
+export default platform('amplitude', {
+  auth: [auth.basic({})],
+  display: {
+    name: 'Amplitude',
+    logos: {
+      defaultURI: fullIcon ?? boxIcon,
+      fullURI: fullIcon,
+      boxURI: boxIcon,
+    },
+    colors: {
+      primary: '#1E61F0',
+    },
+    categories: [],
+  },
+  client,
+  constants: {},
+  actions: {},
+});
