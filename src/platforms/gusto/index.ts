ec3bc4d450f904a863ed12a02ffe7f04f94d32ca
Zachary Kirby <zkirby16@gmail.com>
2023-08-02 23:18:13 -0700

    feat: gusto platform

---
+++ src/platforms/gusto/index.ts
@@ -0,0 +1,27 @@
+import { client } from '@/platforms/gusto/client';
+import fullIcon from '@/platforms/gusto/logos/full';
+import boxIcon from '@/platforms/gusto/logos/box';
+import { auth, platform } from '@/sdk';
+
+export default platform('gusto', {
+  auth: auth.oauth2({
+   authUrl: "https://api.gusto.com/oauth/authorize",
+   tokenUrl: "https://api.gusto.com/oauth/token",
+   questions: [{"id":"subdomain","label":"What is your Gitlab Subdomain?","type":"text"}]
+    }),
+  display: {
+    name: 'Gusto',
+    logos: {
+     defaultURI: fullIcon ?? boxIcon,
+     fullURI: fullIcon,
+     boxURI: boxIcon,
+    },
+    colors: {
+      primary: '#0A8080',
+    },
+    categories: [],
+  },
+  client,
+  constants: {},
+  actions: {},
+});
