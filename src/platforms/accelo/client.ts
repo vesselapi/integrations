4f6572cb59409130f0d60566018eb669ba83e70b
Zachary Kirby <zkirby16@gmail.com>
2023-08-02 19:38:45 -0700

    feat: accelo client

---
+++ src/platforms/accelo/client.ts
@@ -0,0 +1,16 @@
+import { formatUrl, makeRequestFactory } from '@/sdk/client';
+
+const request = makeRequestFactory(async (auth, options) => {
+  return {
+    ...options,
+    url: formatUrl(`https://${answers.subdomain}.api.accelo.com/api/v0`, options.url),
+    headers: {
+      ...options.headers,
+      Authorization: `Bearer ${await auth.getToken()}`,
+    },
+  };
+});
+
+export const client = {
+  passthrough: request.passthrough(),
+};
