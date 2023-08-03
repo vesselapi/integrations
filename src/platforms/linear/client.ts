94cf30fba9a4a98ee1fca0041d08060b66f34dca
Zachary Kirby <zkirby16@gmail.com>
2023-08-02 23:18:19 -0700

    feat: linear client

---
+++ src/platforms/linear/client.ts
@@ -0,0 +1,16 @@
+import { formatUrl, makeRequestFactory } from '@/sdk/client';;
+
+const request = makeRequestFactory(async (auth, options) => {
+  return {
+    ...options,
+    url: formatUrl(`https://api.linear.app`, options.url),
+    headers: {
+      ...options.headers,
+      Authorization: `Bearer ${await auth.getToken()}`,
+    },
+  };
+});
+
+export const client = {
+  passthrough: request.passthrough(),
+};
