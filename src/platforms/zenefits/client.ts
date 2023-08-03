c75789cbd701de019641748a2971f099ccf08f79
Zachary Kirby <zkirby16@gmail.com>
2023-08-03 13:52:04 -0700

    feat: zenefits client

---
+++ src/platforms/zenefits/client.ts
@@ -0,0 +1,16 @@
+import { formatUrl, makeRequestFactory } from '@/sdk/client';
+
+const request = makeRequestFactory(async (auth, options) => {
+  return {
+    ...options,
+url: formatUrl(`https://api.zenefits.com`, options.url),
+    headers: {
+      ...options.headers,
+      Authorization: `Bearer ${await auth.getToken()}`,
+    },
+  };
+});
+
+export const client = {
+  passthrough: request.passthrough(),
+};
