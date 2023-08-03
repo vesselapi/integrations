130e8f88775d56cc62f179c152b5f39d1282e973
Zachary Kirby <zkirby16@gmail.com>
2023-08-02 23:18:14 -0700

    feat: jira client

---
+++ src/platforms/jira/client.ts
@@ -0,0 +1,16 @@
+import { formatUrl, makeRequestFactory } from '@/sdk/client';;
+
+const request = makeRequestFactory(async (auth, options) => {
+  return {
+    ...options,
+    url: formatUrl(`https://jira.atlassian.com/rest/api`, options.url),
+    headers: {
+      ...options.headers,
+      Authorization: `Bearer ${await auth.getToken()}`,
+    },
+  };
+});
+
+export const client = {
+  passthrough: request.passthrough(),
+};
