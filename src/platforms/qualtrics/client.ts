33ff079f59fb485fc177d10594586c68ce6a5ef2
Zachary Kirby <zkirby16@gmail.com>
2023-08-03 15:56:40 -0700

    feat: qualtrics client

---
+++ src/platforms/qualtrics/client.ts
@@ -0,0 +1,17 @@
+import { formatUrl, makeRequestFactory } from '@/sdk/client';
+
+const request = makeRequestFactory(async (auth, options) => {  
+const { answers } = await auth.getMetadata();
+  return {
+    ...options,
+    url: formatUrl(`https://${answers.dc}.qualtrics.com/API/v3`, options.url),
+    headers: {
+      ...options.headers,
+      Authorization: `Bearer ${await auth.getToken()}`,
+    },
+  };
+});
+
+export const client = {
+  passthrough: request.passthrough(),
+};
