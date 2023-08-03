83f5e4b28ec2d583b02a16216995e117de44072f
Zachary Kirby <zkirby16@gmail.com>
2023-08-02 23:17:48 -0700

    feat: front platform

---
+++ src/platforms/front/index.ts
@@ -0,0 +1,26 @@
+import { client } from '@/platforms/front/client';
+import fullIcon from '@/platforms/front/logos/full';
+import boxIcon from '@/platforms/front/logos/box';
+import { auth, platform } from '@/sdk';
+
+export default platform('front', {
+  auth: auth.oauth2({
+   authUrl: "https://app.frontapp.com/oauth/authorize",
+   tokenUrl: "https://app.frontapp.com/oauth/token"
+    }),
+  display: {
+    name: 'Front',
+    logos: {
+     defaultURI: fullIcon ?? boxIcon,
+     fullURI: fullIcon,
+     boxURI: boxIcon,
+    },
+    colors: {
+      primary: '#ffeaf3',
+    },
+    categories: [],
+  },
+  client,
+  constants: {},
+  actions: {},
+});
