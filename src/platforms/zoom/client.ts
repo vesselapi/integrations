50f9250e2054e88de53db99799e776a09279e272
Zachary Kirby <zkirby16@gmail.com>
2023-08-02 23:18:43 -0700

    feat: zoom client

---
+++ src/platforms/zoom/client.ts
@@ -0,0 +1,16 @@
+import { formatUrl, makeRequestFactory } from '@/sdk/client';;
+
+const request = makeRequestFactory(async (auth, options) => {
+  return {
+    ...options,
+    url: formatUrl(`https://api.zoom.us/v2`, options.url),
+    headers: {
+      ...options.headers,
+      Authorization: `Bearer ${await auth.getToken()}`,
+    },
+  };
+});
+
+export const client = {
+  passthrough: request.passthrough(),
+};
