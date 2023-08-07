aaa454899bacf342cf08097cc6a27db072897cd3
Zachary Kirby <zkirby16@gmail.com>
2023-08-07 16:28:56 -0700

    feat: chorus client

---
+++ src/platforms/chorus/client.ts
@@ -0,0 +1,16 @@
+import { formatUrl, makeRequestFactory } from '@/sdk/client';
+
+const request = makeRequestFactory(async (auth, options) => {
+  return {
+    ...options,
+    url: formatUrl(`https://chorus.ai/api`, options.url),
+    headers: {
+      ...options.headers,
+      
+    },
+  };
+});
+
+export const client = {
+  passthrough: request.passthrough(),
+};
